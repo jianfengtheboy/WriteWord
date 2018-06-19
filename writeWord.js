aeb9224aa445d3aa5e2c524b5c3ec8a2472dbba6
jianfengtheboy <jianfengtheboy@163.com>
2018-06-19 11:01:54 +0800

    renew in 6.19

--- writeWord.js
+++ writeWord.js
@@ -1,5 +1,6 @@
-var canvasWidth = 800
+var canvasWidth = Math.min(800,$(window).width() - 20)
 var canvasHeight = canvasWidth
+var strokeColor = "black"
 var isMouseDown = false
 var lastLoc = {x:0,y:0}
 var lastTimestamp = 0
@@ -11,6 +12,7 @@ var context = canvas.getContext("2d")
 canvas.width = canvasWidth
 canvas.height = canvasHeight
 
+$("#controller").css("width",canvasWidth + "px")
 drawGrid()
 
 $("#clear_btn").click(function(e){
@@ -18,45 +20,81 @@ $("#clear_btn").click(function(e){
     drawGrid()
 })
 
+$(".color_btn").click(function(e){
+    $(this).addClass("color_btn_selected").siblings().removeClass("color_btn_selected")
+    strokeColor = $(this).css("background-color")
+})
+
+function beginStroke(point){
+    isMouseDown = true
+    lastLoc = windowToCanvas(point.x,point.y)
+    lastTimestamp = new Date().getTime()
+}
+
+function endStroke(){
+    isMouseDown = false
+}
+
+function moveStroke(point) {
+    var curLoc = windowToCanvas(point.x,point.y)
+    var curTimestamp = new Date().getTime()
+    var s = calcDistance(curLoc,lastLoc)
+    var t = curTimestamp - lastTimestamp
+
+    var lineWidth = calcLineWidth(t,s)
+    //draw
+    context.beginPath()
+    context.moveTo(lastLoc.x,lastLoc.y)
+    context.lineTo(curLoc.x,curLoc.y)
+
+    context.strokeStyle = strokeColor
+    context.lineWidth = lineWidth
+    context.lineCap = "round"
+    context.lineJoin = "round"
+    context.stroke()
+
+    lastLoc = curLoc
+    lastTimestamp = curTimestamp
+    lastLineWidth = lineWidth
+}
+
 canvas.onmousedown = function (e) {
     e.preventDefault();
-    isMouseDown = true
-    lastLoc = windowToCanvas(e.clientX,e.clientY)
-    lastTimestamp = new Date().getTime()
+    beginStroke({x:e.clientX,y:e.clientY})
 }
 canvas.onmouseup = function (e) {
     e.preventDefault();
-    isMouseDown = false
+    endStroke()
 }
 canvas.onmouseout = function (e) {
     e.preventDefault();
-    isMouseDown = false
+    endStroke()
 }
 canvas.onmousemove = function (e) {
     e.preventDefault();
     if(isMouseDown) {
-        var curLoc = windowToCanvas(e.clientX,e.clientY)
-        var curTimestamp = new Date().getTime()
-        var s = calcDistance(curLoc,lastLoc)
-        var t = curTimestamp - lastTimestamp
-
-        var lineWidth = calcLineWidth(t,s)
-        //draw
-        context.beginPath()
-        context.moveTo(lastLoc.x,lastLoc.y)
-        context.lineTo(curLoc.x,curLoc.y)
-
-        context.strokeStyle = "black"
-        context.lineWidth = lineWidth
-        context.lineCap = "round"
-        context.lineJoin = "round"
-        context.stroke()
-
-        lastLoc = curLoc
-        lastTimestamp = curTimestamp
-        lastLineWidth = lineWidth
+        moveStroke({x:e.clientX,y:e.clientY})
     }
 }
+
+canvas.addEventListener("touchstart",function(e){
+    e.preventDefault()
+    touch = e.touches[0]
+    beginStroke({x:touch.pageX,y:touch.pageY})
+})
+
+canvas.addEventListener("touchmove",function(e){
+    e.preventDefault()
+    if(isMouseDown) {
+        touch = e.touches[0]
+        moveStroke({x:touch.pageX,y:touch.pageY})
+    }
+})
+
+canvas.addEventListener("touchend",function(e){
+    e.preventDefault()
+    endStroke()
+})
 
 var maxLineWidth = 30
 var minLineWidth = 1
